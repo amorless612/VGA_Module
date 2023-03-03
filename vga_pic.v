1cccdb86f2767f2e6754439dee055eb391b02ddc
Amor <66984466+amorless612@users.noreply.github.com>
2023-03-03 17:08:04 +0800

    VGA

---
+++ vga_pic.v
@@ -0,0 +1,62 @@
+module vga_pic
+    (
+    input wire                                   vga_clk ,                     //输入工作时钟,频率 25MHz
+    input wire                                   sys_rst_n ,                   //输入复位信号,低电平有效
+    input wire                             [9:0] pix_x ,                       //输入有效显示区域像素点 X 轴坐标
+    input wire                             [9:0] pix_y ,                       //输入有效显示区域像素点 Y 轴坐标
+    
+    output reg                            [11:0] pix_data                      //输出像素点色彩信息
+    
+    );
+
+//********************************************************************//
+//****************** Parameter and Internal Signal *******************//
+//********************************************************************//
+//parameter define
+parameter H_VALID                      = 10'd640 ,                             //行有效数据
+          V_VALID                      = 10'd480 ;                             //场有效数据
+
+parameter RED                          = 12'hf00,                             //红色
+          ORANGE                       = 12'hf80,                             //橙色
+          YELLOW                       = 12'hff0,                             //黄色
+          GREEN                        = 12'h0f0,                             //绿色
+          CYAN                         = 12'h0ff,                             //青色
+          BLUE                         = 12'h00f,                             //蓝色
+          PURPPLE                      = 12'hf0f,                             //紫色
+          BLACK                        = 12'h000,                             //黑色
+          WHITE                        = 12'hfff,                             //白色
+          GRAY                         = 12'h444;                             //灰色
+
+//********************************************************************//
+//***************************** Main Code ****************************//
+//********************************************************************//
+
+//pix_data:输出像素点色彩信息,根据当前像素点坐标指定当前像素点颜色数据
+always@(posedge vga_clk or negedge sys_rst_n)
+begin
+  if(sys_rst_n == 1'b0)
+    pix_data <= 16'd0;
+  else if((pix_x >= 0) && (pix_x < (H_VALID/10)*1))
+    pix_data <= RED;
+  else if((pix_x >= (H_VALID/10)*1) && (pix_x < (H_VALID/10)*2))
+    pix_data <= ORANGE;
+  else if((pix_x >= (H_VALID/10)*2) && (pix_x < (H_VALID/10)*3))
+    pix_data <= YELLOW;
+  else if((pix_x >= (H_VALID/10)*3) && (pix_x < (H_VALID/10)*4))
+    pix_data <= GREEN;
+  else if((pix_x >= (H_VALID/10)*4) && (pix_x < (H_VALID/10)*5))
+    pix_data <= CYAN;
+  else if((pix_x >= (H_VALID/10)*5) && (pix_x < (H_VALID/10)*6))
+    pix_data <= BLUE;
+  else if((pix_x >= (H_VALID/10)*6) && (pix_x < (H_VALID/10)*7))
+    pix_data <= PURPPLE;
+  else if((pix_x >= (H_VALID/10)*7) && (pix_x < (H_VALID/10)*8))
+    pix_data <= BLACK;
+  else if((pix_x >= (H_VALID/10)*8) && (pix_x < (H_VALID/10)*9))
+    pix_data <= WHITE;
+  else if((pix_x >= (H_VALID/10)*9) && (pix_x < H_VALID))
+    pix_data <= GRAY;
+  else
+    pix_data <= BLACK;
+end
+endmodule
---
+++ vga_top.v
@@ -0,0 +1,61 @@
+module vga_colorbar
+    (
+    input  wire                                  sys_clk ,                     //输入工作时钟,频率 50MHz
+    input  wire                                  sys_rst_n ,                   //输入复位信号,低电平有效
+    output wire                                  hsync ,                       //输出行同步信号
+    output wire                                  vsync ,                       //输出场同步信号
+    output wire                           [11:0] rgb                           //输出像素信息
+    );
+//********************************************************************//
+//****************** Parameter and Internal Signal *******************//       //640*480@60
+//********************************************************************//
+//wire define
+wire                                             vga_clk ;                     //VGA 工作时钟,频率 25MHz
+wire                                             locked ;                      //PLL locked 信号
+wire                                             rst_n ;                       //VGA 模块复位信号
+wire                                       [9:0] pix_x ;                       //VGA 有效显示区域 X 轴坐标
+wire                                       [9:0] pix_y ;                       //VGA 有效显示区域 Y 轴坐标
+wire                                      [11:0] pix_data;                     //VGA 像素点色彩信息
+
+//rst_n:VGA 模块复位信号
+assign rst_n            = (sys_rst_n & locked);
+//********************************************************************//
+//*************************** Instantiation **************************//
+//********************************************************************//
+
+//------------- clk_gen_inst -------------
+clk_gen clk_gen_inst
+    (
+    .areset                            (~sys_rst_n                             ), //输入复位信号,高电平有效,1bit
+    .inclk0                            (sys_clk                                ), //输入 50MHz 晶振时钟,1bit
+
+    .c0                                (vga_clk                                ), //输出 VGA 工作时钟,频率 25MHz,1bit
+    .locked                            (locked                                 )  //输出 pll locked 信号,1bit
+    );
+
+//------------- vga_ctrl_inst -------------
+vga_ctrl vga_ctrl_inst
+    (
+    .vga_clk                           (vga_clk                                ), //输入工作时钟,频率 25MHz,1bit
+    .sys_rst_n                         (rst_n                                  ), //输入复位信号,低电平有效,1bit
+    .pix_data                          (pix_data                               ), //输入像素点色彩信息,12bit
+    
+    .pix_x                             (pix_x                                  ), //输出 VGA 有效显示区域像素点 X 轴坐标,10bit
+    .pix_y                             (pix_y                                  ), //输出 VGA 有效显示区域像素点 Y 轴坐标,10bit
+    .hsync                             (hsync                                  ), //输出行同步信号,1bit
+    .vsync                             (vsync                                  ), //输出场同步信号,1bit
+    .rgb                               (rgb                                    )  //输出像素点色彩信息,12bit
+    );
+
+//------------- vga_pic_inst -------------
+vga_pic vga_pic_inst
+    (
+    .vga_clk                           (vga_clk                                ), //输入工作时钟,频率 25MHz,1bit
+    .sys_rst_n                         (rst_n                                  ), //输入复位信号,低电平有效,1bit
+    .pix_x                             (pix_x                                  ), //输入 VGA 有效显示区域像素点 X 轴坐标,10bit
+    .pix_y                             (pix_y                                  ), //输入 VGA 有效显示区域像素点 Y 轴坐标,10bit
+    
+    .pix_data                          (pix_data                               )  //输出像素点色彩信息,12bit   
+    );
+
+ endmodule
